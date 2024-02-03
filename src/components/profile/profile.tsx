8155f68bdd7ab49d202714ae89cfc7b575892b33
MyungWan Park <mywanpark@gmail.com>
2024-02-04 00:01:42 +0900

    feat: profile

---
+++ src/components/profile/profile.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import profilePic from "../../../public/images/profile.jpg";
+import Link from "next/link";
+
+export default function Profile() {
+    return (
+        <article className="flex flex-col items-center">
+            <Image
+                src={profilePic}
+                alt="profile picture"
+                width={200}
+                height={200}
+                className="rounded-full aspect-square"
+            />
+            <h1 className="text-2xl font-bold">Hi, I'm myungwan</h1>
+            <h3 className="text-lg">Full stack engineer</h3>
+            <p>유용한 기능을 개발하는 사람</p>
+            <Link
+                href={"/contact"}
+                className="bg-yellow-600 px-1 rounded-md text-white"
+            >
+                Contact me!
+            </Link>
+        </article>
+    );
+}
